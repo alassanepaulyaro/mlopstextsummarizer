15341aa3edbe390de2ab7ff42201f94dbacee438
alassanepaulyaro <alassane-paul.yaro@outlook.fr>
2025-01-31 17:35:21 +0100

    Data Ingestion Implementation

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.mlopstextsummarizer.logging import logger
+from src.mlopstextsummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("*******************")git add
+STAGE_NAME="Data Ingestion stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
